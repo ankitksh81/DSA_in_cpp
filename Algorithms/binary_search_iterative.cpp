33b461803dfb4778abbd741444d31f2396b6e813
ankitksh81 <ankitksh81@gmail.com>
2021-02-02 16:25:56 +0530

    binary search iterative

--- Algorithms/binary_search_iterative.cpp
+++ Algorithms/binary_search_iterative.cpp
@@ -2,10 +2,11 @@ int binarySearch(int arr[], int key)
 {
     int n = sizeof(arr) / sizeof(arr[0]);
     int low = 0, high = n - 1;
-    int mid = (low + high) / 2;
 
     while (low <= high)
     {
+        int mid = (low + high) / 2;
+
         if (arr[mid] < key)
             high = mid - 1;
         else if (arr[mid] > key)
